c64321cbc31e9c0f76acf6a3548c5a9b52feb278
Sergey42osn <sergey42osn@gmail.com>
2024-11-17 20:05:55 +0700

    123

--- src/components/Gmap.vue
+++ src/components/Gmap.vue
@@ -13,6 +13,7 @@
 <script>
 
 import axios from 'axios'
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 export default {
     name: 'GogleMapCustom',
@@ -27,9 +28,11 @@ export default {
             },
             marker: null,
             markers: [],
+            markersData:[],
             mapMarkers: [],
+            markerCluster: null,
             infoWindow: null,
-            // bounds: new this.google.maps.LatLngBounds(),
+            Mapbounds: new this.google.maps.LatLngBounds(),
             mapOptions: {
                 center: { lat: 53.604, lng: 87.337 },
                 zoomControl: true,
@@ -47,14 +50,81 @@ export default {
             this.Gmap = new this.google.maps.Map(this.$refs.googleMap, {
                 ...this.mapOptions
             });
+
+            this.Gmap.addListener("bounds_changed", () => {
+                // 3 seconds after the center of the map has changed, pan back to the
+                // marker.
+                window.setTimeout(() => {
+                    // this.Gmap.panTo(marker.position);
+
+                    this.getMapBounds();
+
+                }, 3000);
+            });
+
+            this.infoWindow = new this.google.maps.InfoWindow({
+                            content: "",
+                            disableAutoPan: true,
+                        });
+
+            // let markers = this.markers;
+            // let Gmap = this.Gmap;
+
+           this.markerCluster = new MarkerClusterer({});
         },
         getMarkers() {
-            let url = "../assets/markers.js";
+            //let url = "https://localhost:8080/src/assets/markers.js";
+
+            let url = "/js/markers.js";
+
+            console.log(url);
 
             axios.get(url)
                 .then(response => {
                     console.log(response.data);
+                    if (response.data) {
+
+                        this.markersData = response.data;
+
+                        this.markers = this.markersData.map((el) => {
+
+                            const latLng = new this.google.maps.LatLng(el.coords[0], el.coords[1]);
+
+                            const marker = new this.google.maps.Marker({
+                                position: latLng,
+                                map: this.Gmap,
+                            });
+
+                            // markers can only be keyboard focusable when they have click listeners
+                            // open info window when marker is clicked
+                            marker.addListener("click", () => {
+                                this.infoWindow.setContent(latLng.lat + ", " + latLng.lng);
+                                this.infoWindow.open(this.Gmap, marker);
+                            });
+                            return marker;
+                        });
+                    }
                 });
+        },
+        setMarkersMap() {
+            console.log('setMarkers');
+            console.log(this.mapMarkers);
+        },
+        getMarkersMap() {
+            console.log('getMarkersMap');
+            console.log(this.Mapbounds);
+            let m = [];
+            this.markers.forEach(el => {
+                console.log(el.getPosition());
+                if(this.Mapbounds.contains(el.getPosition())){
+                    m.push(el);
+                }
+                this.mapMarkers = m;
+            });
+        },
+        getMapBounds(){
+            this.Mapbounds = this.Gmap.getBounds();
+            this.getMarkersMap();
         }
     },
     created() {
@@ -64,6 +134,11 @@ export default {
         console.log('Gmap mounted');
         console.log(this.google);
         this.initMap();
+    },
+    watch: {
+        mapMarkers: function () {
+            this.setMarkersMap();
+        }
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
+
+axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+};
 
 createApp(App).mount('#app')
